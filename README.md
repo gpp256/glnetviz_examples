4d57aa5ad9dd05afb91a6246f25a3a857274393a
Yoshi <gpp65536@yahoo.co.jp>
2013-06-12 04:51:50 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+glNetViz Examples
+=================
+
+The following are application examples of the glNetViz.  Take a look at the demo:
+
+* [http://gpp256.zapto.org/glNetViz/examples/](http://gpp256.zapto.org/glNetViz/examples/)
+
+e.g.
+
+![http://gpp256.zapto.org/glNetViz/examples/jqplot01_640.png](http://gpp256.zapto.org/glNetViz/examples/jqplot01_640.png)
+![http://gpp256.zapto.org/glNetViz/examples/sdashboard01_640.png](http://gpp256.zapto.org/glNetViz/examples/sdashboard01_640.png)
+
+Setup
+-----
+
+To install, run the following commands.
+
+    e.g.  
+    $ sudo find . -type d -depth 1 \! -name '.*' -exec cp -r {} ${glNetViz_INSTALL_PATH}/examples/ \;
+    $ sudo find  ${glNetViz_INSTALL_PATH}/examples -name "*.cgi" -exec chmod 755 {} \;
+
+Links
+------
+
+users guide(en): http://gpp256.zapto.org/glNetViz/docs/en/html/index.html  
+users guide(ja): http://gpp256.zapto.org/glNetViz/docs/ja/html/index.html  
---
+++ jqplot-example01/event.js
@@ -0,0 +1,268 @@
+/*!
+ * event.js
+ *
+ * Copyright (c) 2013 Yoshi 
+ * This software is distributed under the MIT License.(MIT-LICENSE.txt)
+ */
+
+
+// Initialize global variables
+// parameters for slider ui
+g.globals = {};
+g.scale = 1;
+g.xaxis_rotate_param = 5;
+// parameters for keyboard event
+g.xRot = 0;
+g.xSpeed = 0;
+g.yRot = 0;
+g.ySpeed = -0;
+g.currentlyPressedKeys = {};
+g.lastTime = 0;
+
+// initialize ui
+function initUI() {
+	$(".perspective-enable").click(function(){
+		var parent = $(this).parents('.switch');
+		if (window.confirm('透視投影に切り替えます。よろしいですか?')) {
+			$('.perspective-disable',parent).removeClass('selected');
+			$(this).addClass('selected');
+			togglePerspective(1);
+		} else {
+		}
+	});
+	$(".perspective-disable").click(function(){
+		var parent = $(this).parents('.switch');
+		if (window.confirm('平行投影に切り替えます。よろしいですか?')) {
+			$('.perspective-enable',parent).removeClass('selected');
+			$(this).addClass('selected');
+			togglePerspective(0);
+		} else {
+		}
+	});
+	$('#perspective-disable').addClass('selected');
+	$('#perspective-enable').removeClass('selected');
+	togglePerspective(0);
+	plot();
+}
+
+function plot() {
+  var sdate = '2012/08/24';
+  var edate = '2012/08/31';
+  var xaxisFormat = '%Y/%m/%d %H:%M';
+  var ymax = null;
+  var graph_label = "Load Average (Controller01)";
+  var yaxis_label = "Number of Processes";
+  var line1 = data.line01; var line1_label = "1m";
+  var line2 = data.line02; var line2_label = "5m";
+  var line3 = data.line03; var line3_label = "15m";
+
+  var jqplot = $.jqplot('chart', [line1, line2, line3], {
+    legend: {
+    show:true,
+    placement: 'insideGrid',
+    renderer: jQuery . jqplot . EnhancedLegendRenderer,
+    rendererOptions: { numberRows: 1 },
+    location: 'n'
+    },
+    title: {text: graph_label, fontSize: '12px'},
+    grid: {background:'#f3f3f3', gridLineColor:'#accf9b'},
+    animate: true,
+    animateReplot: true,
+    series: [
+        {label:line1_label, rendererOptions: { smooth: true, animation: { speed: 2500 } }, lineWidth: 0.5, showMarker: false },
+        {label:line2_label, rendererOptions: { smooth: true, animation: { speed: 2500 } }, lineWidth: 0.5, showMarker: false },
+        {label:line3_label, rendererOptions: { smooth: true, animation: { speed: 2500 } }, lineWidth: 0.5, showMarker: false }
+    ],
+    axes: {
+      xaxis:{
+        renderer: $.jqplot.DateAxisRenderer,
+        tickRenderer: $.jqplot.CanvasAxisTickRenderer,
+        min: sdate+' 00:00',
+        max: edate+' 24:00',
+        numberTicks: 7,
+        label: 'Time',
+        labelOptions: { fontSize: '8px', textColor: '#ffffff' },
+        tickOptions: { angle: -20, fontSize: '8px', formatString: xaxisFormat, textColor: '#ffffff' }
+      },
+      yaxis:{
+        labelRenderer: $.jqplot.CanvasAxisLabelRenderer,
+        label: yaxis_label,
+        labelOptions: { fontSize: '8px', textColor: '#ffffff' },
+        tickOptions: { fontSize: '8px' },
+        min:0,
+        max: ymax
+      }
+    },
+    highlighter: {
+      show:true,
+      textColor: '#000000',
+      tooltipLocation: 'n',
+      tooltipAxes: 'y',
+      tooltipSeparator: '',
+      tooltipFormatstring: '<strong>%f</strong>'
+    },
+    cursor:{ show:true, zoom:true }
+  });
+  jqplot.redraw();
+}
+
+// initialize webgl
+function initWebGL (c) {
+	c.width = 700; c.height = 580;
+	// webglコンテキストを取得
+	gl = c.getContext('webgl', { antialias: true }) || 
+		c.getContext('experimental-webgl', { antialias: true });
+    if (!gl) { alert("Error: failed to get webgl context, sorry :-("); return; }
+	// set event listeners
+	c.addEventListener("mousedown", handleMouseDown, false);
+	c.addEventListener("mousemove", handleMouseMove, false);
+	c.addEventListener("mouseup", handleMouseUp, false);
+    document.onkeydown = handleKeyDown;
+    document.onkeyup = handleKeyUp;
+
+	// カリングと深度テストを有効化
+	gl.enable(gl.DEPTH_TEST);
+	gl.depthFunc(gl.LEQUAL);
+	gl.disable(gl.CULL_FACE);
+	gl.disable(gl.BLEND);
+	gl.cullFace(gl.FRONT_AND_BACK);
+}
+
+// create slider ui
+function createSliderUI() {
+	var uiElem = document.getElementById('ui');
+	for (var ii = 0; ii < g_ui.length; ++ii) {
+	  var ui = g_ui[ii];
+	  var obj = g[ui.obj];
+	  obj[ui.name] = ui.value;
+	  var div = document.createElement('div');
+	  setupSlider($, div, ui, obj);
+	  uiElem.appendChild(div);
+	}
+}
+
+// update framerate
+function updateFramerates () {
+	var nowtime = new Date().getTime();
+	var difftime = nowtime - g.rendertime;
+	if (difftime == 0) return;
+	var f = 1000/difftime;
+	g.framerates.push(f); 
+	if (g.framerates.length>10) g.framerates.shift();
+	g.rendertime = nowtime;
+}
+
+// display framerate
+function displayFramerate () {
+	var sum = 0;
+	for (var i = 0; i < g.framerates.length; i++) 
+		sum+= g.framerates[i];
+	var f = Math.round(sum/g.framerates.length);
+	$('#framerate').text('Framerate: '+f+" fps");
+}
+
+// toggle perspective
+function togglePerspective(flag) {
+	g.view_mode = (flag) ? 1 : 0;
+}
+
+// handle key up event
+function handleKeyUp(event) {
+    g.currentlyPressedKeys[event.keyCode] = false;
+}
+
+// handle key down event
+function handleKeyDown(event) {
+    g.currentlyPressedKeys[event.keyCode] = true;
+    if (String.fromCharCode(event.keyCode) == "S") {
+        g.xSpeed = 0; g.ySpeed = 0;
+    }
+}
+
+// handle key events
+function handleKeys() {
+    if (g.currentlyPressedKeys[37]) {
+        // Left cursor key
+        g.ySpeed -= 1;
+    }
+    if (g.currentlyPressedKeys[39]) {
+        // Right cursor key
+        g.ySpeed += 1;
+    }
+    if (g.currentlyPressedKeys[38]) {
+        // Up cursor key
+        g.xSpeed -= 1;
+    }
+    if (g.currentlyPressedKeys[40]) {
+        // Down cursor key
+        g.xSpeed += 1;
+    }
+}
+
+// rotate objects 
+function animate() {
+	var timeNow = new Date().getTime();
+	if (g.lastTime != 0) {
+	    var elapsed = timeNow - g.lastTime;
+	    g.xRot += (g.xSpeed * elapsed) / 1000.0;
+	    g.yRot += (g.ySpeed * elapsed) / 1000.0;
+	}
+	g.lastTime = timeNow;
+}
+
+// set ui parameters
+function setParam(event, ui, obj, name) {
+	var id = event.target.id;
+	var value = ui.value / 1000;
+	if (value < 1) value = 0;
+	if (name == 'Scale') {
+		if (value < 1) value = 1;
+		$('#scale_slider').text($.sprintf(": % 4d", value));
+		g.scale = value;
+	} else if (name == 'Rotation around the x-axis') {
+		$('#xaxis_rotation_slider').text($.sprintf(": % 4d", value));
+		g.xaxis_rotate_param = value;
+	}
+	obj[id] = value;
+}
+
+// get ui values
+function getUIValue(obj, id) {
+	return obj[id] * 1000;
+}
+
+// setup sliders
+function setupSlider($, elem, ui, obj) {
+	var labelDiv = document.createElement('div');
+	labelDiv.appendChild(document.createTextNode(ui.name));
+	var sliderDiv = document.createElement('div');
+	sliderDiv.id = ui.name;
+	elem.appendChild(labelDiv);
+	elem.appendChild(sliderDiv);
+	$(sliderDiv).slider({
+		range: false,
+		step: 1,
+		max: ui.max * 1000,
+		min: ui.min,
+		value: getUIValue(obj, ui.name),
+		slide: function(event, ui) { setParam(event, ui, obj, sliderDiv.id); }
+	});
+	$(sliderDiv).css({"margin-bottom" : "6px"});
+	
+	if (ui.name == 'Scale') {
+		$('#scale_slider').text($.sprintf(": % 4d", ui.value));
+		g.scale = ui.value;
+	} else if (ui.name == 'Rotation around the x-axis') {
+		$('#xaxis_rotation_slider').text($.sprintf(": % 4d", ui.value));
+		g.xaxis_rotate_param = ui.value;
+	}
+}
+
+function changeNetworkTopologies(sb) {
+	g.getstat_counter = 470;
+	getSdnObjects(sb.value);
+	getOtherObjects(sb.value);
+	getFlowData(sb.value);
+	g.flowdata_id = sb.value;
+	//g.getflow_updateflag = 1;
+}
